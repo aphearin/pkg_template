cd5d643c9ad585a5aa0592acf48cf9ac5843f089
Andrew Hearin <ahearin@anl.gov>
2021-05-21 09:47:47 -0500

    Updated script to include setup.cfg

--- scripts/format_python_package.py
+++ scripts/format_python_package.py
@@ -6,10 +6,12 @@ import subprocess
 
 
 def _replace_pkg_template_with_pkgname(pkg_fname, new_pkgname):
-    with open(pkg_fname, 'r') as f:
-        new_raw_lines = [raw_line.replace('pkg_template', new_pkgname) for raw_line in f]
+    with open(pkg_fname, "r") as f:
+        new_raw_lines = [
+            raw_line.replace("pkg_template", new_pkgname) for raw_line in f
+        ]
 
-    with open(pkg_fname, 'w') as f:
+    with open(pkg_fname, "w") as f:
         for line in new_raw_lines:
             f.write(line)
 
@@ -37,20 +39,23 @@ if __name__ == "__main__":
     cmd = "cp pkg_template/setup.py {0}".format(root_dirname_pkg)
     __ = subprocess.check_output(cmd, shell=True)
 
+    cmd = "cp pkg_template/setup.cfg {0}".format(root_dirname_pkg)
+    __ = subprocess.check_output(cmd, shell=True)
+
     cmd = "cp pkg_template/.gitignore {0}".format(root_dirname_pkg)
     __ = subprocess.check_output(cmd, shell=True)
 
     cmd = "cp -r pkg_template/pkg_template {0}".format(root_dirname_pkg)
     __ = subprocess.check_output(cmd, shell=True)
 
-    name1 = os.path.join(root_dirname_pkg, 'pkg_template')
+    name1 = os.path.join(root_dirname_pkg, "pkg_template")
     name2 = os.path.join(root_dirname_pkg, pkgname)
     cmd = "mv {0} {1}".format(name1, name2)
     __ = subprocess.check_output(cmd, shell=True)
 
     #  Search for 'pkg_template' and replace with pkgname in the following files:
-    setup_fname = os.path.join(root_dirname_pkg, 'setup.py')
-    readme_fname = os.path.join(root_dirname_pkg, 'README.md')
+    setup_fname = os.path.join(root_dirname_pkg, "setup.py")
+    readme_fname = os.path.join(root_dirname_pkg, "README.md")
     fnames_to_modify = [setup_fname, readme_fname]
     for fname in fnames_to_modify:
         _replace_pkg_template_with_pkgname(fname, pkgname)
