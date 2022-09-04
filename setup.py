ed68931d6e995bd16a314231d2d1fb37d779d4ab
Andrew Hearin <ahearin@anl.gov>
2022-09-04 06:18:14 -0500

    Improve versioning system

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
+import os
 from setuptools import setup, find_packages
 
 
 PACKAGENAME = "pkg_template"
-VERSION = "0.0.dev"
+__version__ = None
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "pkg_template", "_version.py"
+)
+with open(pth, "r") as fp:
+    exec(fp.read())
 
 
 setup(
     name=PACKAGENAME,
-    version=VERSION,
+    version=__version__,
     author="Andrew Hearin",
     author_email="ahearin@anl.gov",
     description="Some package",
     long_description="Just some package",
     install_requires=["numpy"],
     packages=find_packages(),
-    url="https://github.com/aphearin/pkg_template"
+    url="https://github.com/aphearin/pkg_template",
+    package_data={"pkg_template": ("tests/testing_data/*.dat",)},
 )
