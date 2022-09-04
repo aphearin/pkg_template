9e0782131634d3cea44b0828f5dd8142d3c9f2c5
Andrew Hearin <ahearin@anl.gov>
2022-09-04 07:47:28 -0500

    Add testing notes to README

--- scripts/format_python_package.py
+++ scripts/format_python_package.py
@@ -30,6 +30,22 @@ To install pkg_template into your environment from the source code:
 $ cd /path/to/root/pkg_template
 $ python setup.py install
 ```
+
+## Testing
+To run the suite of unit tests:
+
+```
+$ cd /path/to/root/pkg_template
+$ pytest
+```
+
+To build html of test coverage:
+
+```
+$ pytest -v --cov --cov-report html
+$ open htmlcov/index.html
+```
+
 """
 
 if __name__ == "__main__":
